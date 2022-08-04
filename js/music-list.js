d2d7b05e919cf00bb41a9d22dc92f8351b164171
nyeinswe <winnaingswe1331@gmail.com>
2022-08-04 11:06:53 +0630

    another features

--- js/music-list.js
+++ js/music-list.js
@@ -51,13 +51,7 @@ let allMusic = [
  
 
 
-  // like this paste it and remember to give comma after ending of this bracket }
-  // {
-  //   name: "Here is the music name",
-  //   artist: "Here is the artist name",
-  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-  // }
+
    {
     name: "Ko Si Moh Yay Yar",
     artist: "Derek Millar",
