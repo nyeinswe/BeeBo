c76116cda78a9236f6d76801e54b8dd0d6757d1e
nyeinswe <winnaingswe1331@gmail.com>
2022-08-04 10:40:22 +0630

    site alive

---
+++ js/music-list.js
@@ -0,0 +1,79 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "အချစ်ဦးမို့မေ့မရပါ",
+    artist: "Soe Lwin Lwin",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "လမ်း​ပေါ်ကလူ",
+    artist: "Soe Lwin Lwin",
+    img: "music-2",
+    src: "music-2"
+  },
+    {
+    name: "နေနိုင်သူလေး",
+    artist: "Soe Lwin Lwin",
+    img: "music-3",
+    src: "music-3"
+  },
+
+    {
+    name: "ချိုပေါ့ကျ",
+    artist: "Aung Yin",
+    img: "music-4",
+    src: "music-4"
+  },
+
+    {
+    name: "အိမ်မက်ထဲကရထားကြီး",
+    artist: "Aung Yin",
+    img: "music-5",
+    src: "music-5"
+  },
+    {
+    name: "ချစ်",
+    artist: "Aung Yin",
+    img: "music-6",
+    src: "music-6"
+  },
+ 
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+   {
+    name: "Ko Si Moh Yay Yar",
+    artist: "Derek Millar",
+    img: "music-7",
+    src: "music-7"
+  },
+      {
+    name: "စိမ်းရက်လေအား",
+    artist: "ထူးအိမ်သင်",
+    img: "music-8",
+    src: "music-8"
+  },
+        {
+    name: "ရက်စက်လိုက်တာ",
+    artist: "ထူးအိမ်သင်",
+    img: "music-9",
+    src: "music-9"
+  },
+];
